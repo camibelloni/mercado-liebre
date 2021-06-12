55710361194b0dfd6e86db6449b353115ec6903a
camibelloni <camibelloni@gmail.com>
2021-06-12 19:13:01 -0300

    Mi primer commmit del proyecto

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => console.log('Escuchando en el puerto 3000'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+app.get('/ofertas', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/ofertas.html'));
+})
+
+app.get('/ayuda', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/ayuda.html'));
+})
+
+app.get('/crear-cuenta', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/crear-cuenta.html'));
+})
+
+app.get('/ingresa', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/ingresa.html'));
+})
+
+app.get('/mis-compras', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/mis-compras.html'));
+})
+
+app.get('/tiendas-oficiales', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/tiendas-oficiales.html'));
+})
+
+app.get('/vender', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/vender.html'));
+})
